885e9d71a20aa4dfe736294902c57fffee4d7c19
Gustavo Rohrer <gustavo.rohrer@intive.com>
2020-04-14 16:34:02 -0300

    Main.java cleanup

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(SecurityConfig.class)
 public class Main {
 
     public static void main(String[] args) {
