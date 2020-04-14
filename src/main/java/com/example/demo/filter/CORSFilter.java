1a29c2d5bde14b7225b5c3c0a7b17f3d50cd7ee1
Gustavo Rohrer <gustavo.rohrer@intive.com>
2020-04-14 16:33:40 -0300

    websocket config, chat message model and controller, CORS filter

---
+++ src/main/java/com/example/demo/filter/CORSFilter.java
@@ -0,0 +1,49 @@
+package com.example.demo.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CORSFilter implements Filter {
+
+    private final List<String> allowedOrigins = Arrays.asList("http://localhost:1234");
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+            HttpServletRequest request = (HttpServletRequest) req;
+            HttpServletResponse response = (HttpServletResponse) res;
+
+            String origin = request.getHeader("Origin");
+            response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+            response.setHeader("Vary", "Origin");
+
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+}
