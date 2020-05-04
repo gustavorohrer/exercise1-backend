332aa4f75b6ff59c9ed230cd9647558f5788742c
Gustavo Rohrer <gustavo.rohrer@intive.com>
2020-05-04 08:34:13 -0300

    WebSocketEventListener changes

--- src/main/java/com/example/demo/controllers/WebSocketEventListener.java
+++ src/main/java/com/example/demo/controllers/WebSocketEventListener.java
@@ -1,44 +1,34 @@
 package com.example.demo.controllers;
 
-import com.example.demo.model.ChatMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class WebSocketEventListener {
 
+    public static List<String> sessions = new ArrayList<>();
+
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
-
-    private final SimpMessageSendingOperations messagingTemplate;
-
-    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
         logger.info("Received a new web socket connection");
+        String simpSessionId = (String) event.getMessage().getHeaders().get("simpSessionId");
+        //TODO OGet user data from message headers
+        sessions.add(simpSessionId);
     }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
-        if (username != null) {
-            logger.info("User Disconnected : " + username);
-
-            ChatMessage chatMessage = new ChatMessage();
-            chatMessage.setType(ChatMessage.MessageType.LEAVE);
-            chatMessage.setSender(username);
-
-            messagingTemplate.convertAndSend("/topic/public", chatMessage);
-        }
+        String simpSessionId = (String) event.getMessage().getHeaders().get("simpSessionId");
+        sessions.remove(simpSessionId);
+        logger.info("User with session id: " + simpSessionId + " disconnected");
     }
 }
